85addbaf5d1ef5d2e8710949d636f0e6ff5fbeb5
IsuruPradeep <pisuru136@gmail.com>
2025-06-26 19:59:35 +0530

    server runinerrorsg without

--- server/index.js
+++ server/index.js
@@ -2,9 +2,10 @@ import express from "express"
 import mongoose from "mongoose"
 import bodyParser from "body-parser"
 import dotenv from "dotenv"
+import route from "./routes/userRoute.js"
 
 const app= express();
-app.use(bodyParser.jason());
+app.use(bodyParser.json());
 dotenv.config();
 
 const PORT=process.env.PORT || 7000;
@@ -19,3 +20,5 @@ mongoose
         })
     })
     .catch((error) => console.log(error));
+
+    app.use("/api", route);
