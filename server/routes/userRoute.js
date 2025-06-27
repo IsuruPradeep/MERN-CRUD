09289e04d59db630819958be21bd014ed410214b
IsuruPradeep <pisuru136@gmail.com>
2025-06-28 01:12:05 +0530

    get user by specific id

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,8 +1,10 @@
 import  express  from 'express';
-import { create, getAllUSers } from '../controller/userController.js';
+import { create, getAllUSers, getUserById } from '../controller/userController.js';
 
 const route = express.Router();
 
-route.post("/user", create)
+route.post("/user", create);
 route.get("/users",getAllUSers);
+route.get("/user/:id", getUserById)
+
 export default route;
