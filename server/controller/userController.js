577d7bc073c465e6e9d30e4b4fcf190d838741ab
IsuruPradeep <pisuru136@gmail.com>
2025-06-28 01:24:43 +0530

    update the user by id

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,43 +1,60 @@
 import User from "../model/userModel.js"
 
 
-export const create = async(req, res) => {
+export const create = async (req, res) => {
     try {
         const newUser = new User(req.body);
-        const {email} = newUser;
-        const userExist = await User.findOne({email})
-        if(userExist){
-            return res.status(400).json({message: "USer already exists."});
+        const { email } = newUser;
+        const userExist = await User.findOne({ email })
+        if (userExist) {
+            return res.status(400).json({ message: "USer already exists." });
         }
         const savedData = await newUser.save();
         res.status(200).json(savedData);
     } catch (error) {
-        res.status(500).json({errorMessage:error.message})
+        res.status(500).json({ errorMessage: error.message })
     }
 };
 
-export const getAllUSers= async (req, res) => {
+export const getAllUSers = async (req, res) => {
     try {
         const userData = await User.find();
-        if (!userData|| userData.length=== 0){
-            return res.status(404).json({message: "User data not found."});
+        if (!userData || userData.length === 0) {
+            return res.status(404).json({ message: "User data not found." });
         }
         res.status(200).json(userData);
-    } catch (error){
-        res.status(500).json({errorMessage:error.message});
+    } catch (error) {
+        res.status(500).json({ errorMessage: error.message });
 
     }
 };
 
-export const getUserById = async (req,res) => {
+export const getUserById = async (req, res) => {
     try {
         const id = req.params.id;
-        const userExist =  await User.findById(id);
-        if (!userExist){
-            return res.status(404).json({message: "User not found."});
+        const userExist = await User.findById(id);
+        if (!userExist) {
+            return res.status(404).json({ message: "User not found." });
         }
         res.status(200).json(userExist)
     } catch (error) {
-        res.status(500).json({errorMessage: error.message});
+        res.status(500).json({ errorMessage: error.message });
     }
 };
+
+export const update = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const userExist = await User.findById(id);
+        if (!userExist) {
+            return res.status(404).json({ message: "User not found." });
+        }
+        const updateData= await User.findByIdAndUpdate(id, req.body,{
+            new:true
+        })
+        res.status(200).json(updateData)
+    } catch (error) {
+        res.status(500).json({ errorMessage: error.message });
+
+    }
+}
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,10 +1,10 @@
 import  express  from 'express';
-import { create, getAllUSers, getUserById } from '../controller/userController.js';
+import { create, getAllUSers, getUserById, update } from '../controller/userController.js';
 
 const route = express.Router();
 
 route.post("/user", create);
 route.get("/users",getAllUSers);
-route.get("/user/:id", getUserById)
-
+route.get("/user/:id", getUserById);
+route.put("/update/user/:id",update)
 export default route;
